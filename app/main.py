97c3afe2e2798293f4269d64c9400dc7cd9ae8c5
mbusbee <mbusbee@gmail.com>
2023-12-28 20:33:57 -0500

    built docker app

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+import pandas as pd
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    df = pd.read_csv(file.file)
+    # Process the DataFrame (df) as needed
+    return {"filename": file.filename, "columns": df.columns.tolist()}
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
